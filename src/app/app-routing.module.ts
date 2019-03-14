5ee9cc879941a4ee07f80b5c3291844a0b2a3d13
Diogo Pires <dc.pires@campus.fct.unl.pt>
2019-03-14 14:40:42 +0000

    photo test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,12 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'photo-home',
     pathMatch: 'full'
   },
   {
-    path: 'home',
-    loadChildren: './home/home.module#HomePageModule'
+    path: 'photo-home',
+    loadChildren: './home_photo/photo-home.module#PhotoHomePageModule'
   },
   {
     path: 'list',
